dd4de97da511b38904640f7806d54609c0a76037
Eddie Castillo <46185445+eddiecasti280@users.noreply.github.com>
2026-01-15 18:04:30 -0600

    Changes

--- percepty/src/components/Navigation.js
+++ percepty/src/components/Navigation.js
@@ -4,18 +4,18 @@ import colors from '../utils/constants';
 
 function Navigation() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: colors.taupe_gray, paddingLeft: '10px' }}>
-      <a className="navbar-brand" href="/">Percepty</a>
+    <nav className="navbar navbar-expand-lg navbar-light custom-navbar shadow-sm" style={{ backgroundColor: colors.taupe_gray, paddingLeft: '10px', position: 'sticky', top: 0, zIndex: 1000 }}>
+      <Link className="navbar-brand" to="/">Percepty</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <button className="nav-link dropdown-toggle btn btn-link" id="navbarDropdownMath" data-bs-toggle="dropdown" aria-expanded="false" style={{textDecoration: 'none'}}>
               Mathematics
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMath">
                 <li><Link className='dropdown-item' to='/mathOverview'>Overview</Link></li>
                 <li><Link className="dropdown-item" to="/integration">Integration</Link></li>
                 <li><Link className="dropdown-item" to="/derivatives">Derivatives</Link></li>
@@ -23,14 +23,16 @@ function Navigation() {
           </li>
           {/* Add other links here */}
           <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <button className="nav-link dropdown-toggle btn btn-link" id="navbarDropdownProg" data-bs-toggle="dropdown" aria-expanded="false" style={{textDecoration: 'none'}}>
               Programming
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdownProg">
               {/* <li><Link className="dropdown-item" to="/integration">Python</Link></li> */}
               <li className="dropdown-submenu">
-                <a className="dropdown-item dropdown-toggle" href="#">Python</a>
-                <ul className="dropdown-menu">
+                <button className="dropdown-item dropdown-toggle btn btn-link" id="pythonDropdown" data-bs-toggle="dropdown" aria-expanded="false" style={{textDecoration: 'none'}}>
+                  Python
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="pythonDropdown">
                   <li><Link className="dropdown-item" to="/python/basic">Basic</Link></li>
                   <li><Link className="dropdown-item" to="/python/advanced">Advanced</Link></li>
                 </ul>
@@ -39,10 +41,10 @@ function Navigation() {
             </ul>
           </li>
           <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <button className="nav-link dropdown-toggle btn btn-link" id="navbarDropdownLing" data-bs-toggle="dropdown" aria-expanded="false" style={{textDecoration: 'none'}}>
               Linguistics
-            </a>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdownLing">
               <li><Link className="dropdown-item" to="/linguistics/alphaSyll">Alphabets and Syllabaries</Link></li>
               <li><Link className="dropdown-item" to="/linguistics/theory">Theory</Link></li>
             </ul>
